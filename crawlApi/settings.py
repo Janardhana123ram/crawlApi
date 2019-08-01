51a47cf851117b14ea4d668b36f5ac69011561b2
Janardhana <janardhan321ram@gmail.com>
2019-08-01 14:03:51 +0530

    updated

--- crawlApi/settings.py
+++ crawlApi/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_p58%pl6v4y!d2qnp+(mtk63i88kav
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-#ALLOWED_HOSTS = ['warm-atoll-21342.herokuapp.com']
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS = ['warm-atoll-21342.herokuapp.com']
+#ALLOWED_HOSTS=[]
 
 # Application definition
 
